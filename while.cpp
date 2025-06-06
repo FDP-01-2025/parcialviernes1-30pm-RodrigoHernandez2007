ec1332e65c7523ba544a64747e5fbb5a98138901
RodrigoHernandez2007 <00207525@uca.edu.sv>
2025-06-06 14:51:51 -0600

    Parcial

--- while.cpp
+++ while.cpp
@@ -7,11 +7,15 @@ int num;
 cout<<"Ingrese un numero del 1 al 10."<<endl;
 cin>>num;
 
-while (num < 1 && num > 10)
+do
 {
-         cout << "El numero: "<< num << " no esta entre 1 y 10, vuelve a interntarlo de nuevo."<<endl;
-         cin>>num;
+    cout<<"Ingrese un numero del 1 al 10."<<endl;
+cin>>num;
+if (num <1 || num > 10){
+
+    cout << "El numero: "<< num << " no esta entre 1 y 10, vuelve a interntarlo de nuevo."<<endl;
 }
+} while (num <1 || num > 10);
     
 
 cout <<"El numero: " << num << " esta entre el 1 y 10 bien hecho";
